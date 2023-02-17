894e8f772001cee1ed8f2dff7ebaa9fa7b75dceb
Omeritzio <omershlomo441@gmail.com>
2023-02-17 14:59:39 +0200

    first commit

--- app.js
+++ app.js
@@ -31,8 +31,6 @@ async function main() {
     async function listDatabases(client){
         databasesList = await client.db().admin().listDatabases();
      
-        console.log("Databases:");
-        databasesList.databases.forEach(db => console.log(` - ${db.name}`));
     };
     await mongoose.connect(uri);
 }
