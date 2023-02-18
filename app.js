ae127edde636f7795b6b4bf59c01016a6b0a6eb0
Omeritzio <omershlomo441@gmail.com>
2023-02-19 01:49:59 +0200

    first commit

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ main().catch(err => console.log(err));
 
 async function main() {
     mongoose.set("strictQuery", false);
-    const uri = "mongodb+srv://ToDoBom:WetgVPwRGY0DuJCG@todobom.3bcl7nh.mongodb.net/ToDoBom?retryWrites=true&w=majority";
+    const uri = "mongodb+srv://ToDoBom:WetgVPwRGY0DuJCG@todobom.ohp9sli.mongodb.net/ToDoBom?retryWrites=true&w=majority";
+    
     const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
     client.connect(err => {
         const collection = client.db("ToDoBom").collection("items");
